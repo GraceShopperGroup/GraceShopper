f75320f4fec48b6889b5754bea1ae792694c706d
Shayan Sheikh <najam.shayan@gmail.com>
2018-01-09 16:40:30 -0500

    Added Review model:

---
+++ server/db/models/review.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Review = db.define('review', {
+  content: {
+    type: Sequelize.TEXT,
+    validate: {
+      len: [50, 500]
+    }
+  }
+})
+
+module.exports = Review
