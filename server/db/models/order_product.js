4fb3d014a621e6f02f23f54917c0bc79b6e834e4
sk521 <samkwon521@gmail.com>
2018-01-10 18:30:45 -0500

    category table created

--- server/db/models/order_product.js
+++ server/db/models/order_product.js
@@ -4,7 +4,7 @@ const db = require('../db')
 const OrderProduct = db.define('order_product', {
   priceAtPurchase: {
     type: Sequelize.INTEGER
-  }, 
+  },
   quantity: {
     type: Sequelize.INTEGER
   }
