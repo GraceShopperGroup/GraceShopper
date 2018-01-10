b7b714e45daeeb757ead88a9ed982e85638b2af0
Shayan Sheikh <najam.shayan@gmail.com>
2018-01-10 15:18:50 -0500

    Removed extraneous comments in order model

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,16 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-// OB/DK: many<->many relationship, consider a custom join table, order_products (this would keep the foreign key relationships "first class")
-/*
-const Order = db.define('order', ...);
-const Product = db.define('product', ...);
-const OrderProduct = db.define('order_product', {
-  priceAtPurchaseTime: Sequelize.DECIMAL,
-  quantity: Sequelize.INTEGER
-});
-Order.belongsToMany(Product, {through: OrderProduct});
-*/
 const Order = db.define('order', {
   
 })
