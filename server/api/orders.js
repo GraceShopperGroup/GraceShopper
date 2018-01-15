d8d44a2a8bcdcd34d97c7bf7f37a3bcafc384d30
Shayan Sheikh <najam.shayan@gmail.com>
2018-01-15 16:40:11 -0500

    Added order route to get all orders for a particular user

--- server/api/orders.js
+++ server/api/orders.js
@@ -2,12 +2,17 @@ const router = require('express').Router();
 const { User } = require('../db/models');
 
 router.get('/', (req, res, next) => {
+  //console.log(req);
   if (req.user && req.user.id) {
+    console.log('true')
     User.findById(req.user.id)
       .then(user => user.getOrders())
       .then(orders => res.json(orders))
       .catch(next)
   } else {
+    console.log('false')
     res.status(404);
   }
 })
+
+module.exports = router;
