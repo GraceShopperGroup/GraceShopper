f5f0221a9a115966dc8a697fdf83bfb4ff52ce9c
Shayan Sheikh <najam.shayan@gmail.com>
2018-01-16 14:37:50 -0500

    Added thunk to orders reducer for making orders

--- client/store/reducers/orders.js
+++ client/store/reducers/orders.js
@@ -3,10 +3,13 @@ import axios from 'axios';
 
 //ACTION TYPE
 const GET_ORDERS = 'GET_ORDERS';
+const POST_ORDER = 'POST_ORDER';
 
 
 // //ACTION CREATOR
-export function getOrders(orders) { return { type: GET_ORDERS, orders}}
+export function getOrders(orders) { return { type: GET_ORDERS, orders} }
+export function postOrder(order) { return { type: POST_ORDER, order} }
+
 // //THUNK CREATOR
 
 export function fetchOrders() {
@@ -21,12 +24,22 @@ export function fetchOrders() {
   }
 }
 
+export function makeOrder() {
+  return function thunk(dispatch) {
+    return axios.post('/api/orders')
+      .then(res => res.data)
+      .then(order => dispatch(postOrder(order)))
+  }
+}
+
 // //REDUCER
 // Never directly mutate the state, always copy old data
 export default function reducer(state = [], action) {
   switch (action.type) {
     case GET_ORDERS:
       return action.orders
+    case POST_ORDER:
+      return [...state, action.order]
     default:
       return state;
   }
