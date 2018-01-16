8f6aaefa5d7bcd429723a6075925aaeaefd0e006
Shayan Sheikh <najam.shayan@gmail.com>
2018-01-16 11:43:00 -0500

    Added post order route to be used when checking out'

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Product } = require('../db/models');
+const { User, Product, Order, OrderProduct } = require('../db/models');
 
 router.get('/', (req, res, next) => {
   if (req.user && req.user.id) {
@@ -9,6 +9,28 @@ router.get('/', (req, res, next) => {
       }))
       .then(orders => res.json(orders))
       .catch(next)
+  } else {
+    res.status(404);
+  }
+})
+
+router.post('/', (req, res, next) => {
+  if (req.user && req.user.id && req.session.cart) {
+    Order.create()
+      .then(order => {
+        let orderProducts = [];
+        Object.keys(req.session.cart).forEach(prod => {
+          orderProducts.push({
+            orderId: order.id,
+            productId: prod.id,
+            quantity: prod.quantity,
+            priceAtPurchase: prod.price
+          })
+        })
+        return OrderProduct.create(orderProducts)
+      })
+      .then((orderProducts) => res.json(orderProducts))
+      .catch(err => console.log(err))
   } else {
     res.status(404);
   }
