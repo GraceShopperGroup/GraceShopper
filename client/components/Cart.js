8e1c9aa9ee079c666a921821fa63cf840eab61d1
sk521 <samkwon521@gmail.com>
2018-01-16 12:47:27 -0500

    cart-css fin

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -5,23 +5,50 @@ import { removeItem } from '../store'
 //Stateless Component
 const Cart = (props) => {
   const { cart, removeFromCart } = props;
+  let total = 0;
   return (
     <div>
-      <div className="header-title">
+      <div className="header-title text-center">
         <h2 className="heading-main">Shopping Cart</h2>
       </div>
-      <ul>
-        {
-          Object.keys(cart).map((id) => {
-            return (
-              <li key={id}>{cart[id].name} Quantity:{cart[id].quantity}
-                Price: {cart[id].quantity * cart[id].price}
-                <button onClick={() => removeFromCart(id)}>X</button>
-              </li>
-            )
-          })
-        }
-      </ul>
+      <table className="cart-table full table--responsive">
+        <thead className="cart-row cart-header-labels">
+          <tr>
+            <th colSpan="2" className="text-center">Product</th>
+            <th className="text-center apph3">Price</th>
+            <th className="text-center apph3">Quantity</th>
+            {/* <th className="text-right apph3">Total</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Object.keys(cart).map((id) => {
+              total += cart[id].quantity * cart[id].price
+              return (
+                <tr className="cart-row table-section" key={id}>
+                  <td className="remove-button">
+                    <button onClick={() => removeFromCart(id)}>X</button>
+                  </td>
+                  <td className="cart-product prod-name">
+                    {cart[id].name}
+                  </td>
+                  <td className="cart-product prod-price">
+                    ${parseFloat(cart[id].price).toFixed(2)}
+                  </td>
+                  <td className="cart-product prod-quantity">
+                    {cart[id].quantity}
+                  </td>
+                </tr>
+                )
+              })
+            }
+        </tbody>
+      </table>
+      <div id="tfooter">
+        <h4>
+          TOTAL: ${total.toFixed(2)}
+        </h4>
+      </div>
     </div>
   )
 }
